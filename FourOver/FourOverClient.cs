da5503517a6367b7204a1316274af628c63df201
William Barrett Simms <wbsimms@gmail.com>
2014-02-08 12:13:46 -0500

    push

--- FourOver/FourOverClient.cs
+++ FourOver/FourOverClient.cs
@@ -97,6 +97,8 @@ namespace FourOver
                 items.RemoveAt(0);
                 foreach (string runsize in items)
                 {
+                    int parsed = Convert.ToInt32(runsize);
+                    if (parsed >= 10000) continue;
                     runsizeList.Select(runsize);
                     browser.WaitForComplete();
                     SelectList colorList = browser.SelectList(Find.ById("color"));
